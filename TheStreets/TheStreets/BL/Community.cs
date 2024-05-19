1a106ed1ca35e28622cf6e5af054360612b01833
DrorCohen123 <158835481+drornivtal@users.noreply.github.com>
2024-05-19 14:36:52 +0300

    Add project files.

---
+++ TheStreets/TheStreets/BL/Community.cs
@@ -0,0 +1,34 @@
+﻿namespace TheStreets.BL
+{
+    public class Community
+    {
+        int communityId;
+        string name;
+        string city;
+        string location;
+        string discription;
+        string primaryPic;
+        static List<Community> communitiesList = new List<Community>();
+
+        public Community() { }
+        public Community(int communityId, string name, string city, string location, string discription, string primaryPic)
+        {
+            this.communityId = communityId;
+            this.name = name;
+            this.city = city;
+            this.location = location;
+            this.discription = discription;
+            this.primaryPic = primaryPic;
+        }
+
+        public int Insert()
+        {
+
+            return 1;
+        }
+        public List<Community> Read()
+        {
+            return communitiesList;
+        }
+    }
+}
---
+++ TheStreets/TheStreets/Controllers/CommunitisController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheStreets.BL;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TheStreets.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommunitisController : ControllerBase
+    {
+        // GET: api/<CommunitisController>
+        [HttpGet]
+        public IEnumerable<Community> Get()
+        {
+            Community community = new Community();
+            return community.Read();
+        }
+
+        // GET api/<CommunitisController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CommunitisController>
+        [HttpPost]
+        public void Post([FromBody] Community community)
+        {
+            community.Insert();
+        }
+
+        // PUT api/<CommunitisController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CommunitisController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
